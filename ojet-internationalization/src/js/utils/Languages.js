741c4726374b3281ece0418e380437943b5aae98
Daniel Merchán García <daniel.merchan.garcia@gmail.com>
2019-02-14 11:03:25 +0100

    OJET Localization - Alpha not working

---
+++ ojet-internationalization/src/js/utils/Languages.js
@@ -0,0 +1,115 @@
+define(() => {
+    const Languages = {
+        supportedLanguages: [
+            {
+                name: 'Arabic',
+                locale: 'ar'
+            },
+            {
+                name: 'Brazilian Portuguese',
+                locale: 'pt'
+            },
+            {
+                name: 'Canadian French',
+                locale: 'fr-CA'
+            },
+            {
+                name: 'Czech',
+                locale: 'cs'
+            },
+            {
+                name: 'Danish',
+                locale: 'da'
+            },
+            {
+                name: 'Dutch',
+                locale: 'nl'
+            },
+            {
+                name: 'Finnish',
+                locale: 'fi'
+            },
+            {
+                name: 'French',
+                locale: 'fr'
+            },
+            {
+                name: 'German',
+                locale: 'de'
+            },
+            {
+                name: 'Greek',
+                locale: 'el'
+            },
+            {
+                name: 'Hebrew',
+                locale: 'he'
+            },
+            {
+                name: 'Hungarian',
+                locale: 'hu'
+            },
+            {
+                name: 'Italian',
+                locale: 'it'
+            },
+            {
+                name: 'Japanese',
+                locale: 'jp'
+            },
+            {
+                name: 'Korean',
+                locale: 'ko'
+            },
+            {
+                name: 'Norwegian',
+                locale: 'no'
+            },
+            {
+                name: 'Polish',
+                locale: 'pl'
+            },
+            {
+                name: 'Portuguese',
+                locale: 'pt-PT'
+            },
+            {
+                name: 'Romania',
+                locale: 'ro'
+            },
+            {
+                name: 'Russian',
+                locale: 'ru'
+            },
+            {
+                name: 'Simplified Chinese',
+                locale: 'zh-CN'
+            },
+            {
+                name: 'Slovak',
+                locale: 'sk'
+            },
+            {
+                name: 'Spanish',
+                locale: 'es'
+            },
+            {
+                name: 'Swedish',
+                locale: 'sv'
+            },
+            {
+                name: 'Thai',
+                locale: 'th'
+            },
+            {
+                name: 'Traditional Chinese',
+                locale: 'zh-TW'
+            },
+            {
+                name: 'Turkish',
+                locale: 'tr'
+            }
+        ],
+    }
+    return Languages;
+});
